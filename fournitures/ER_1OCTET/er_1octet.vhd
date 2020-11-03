24103e519860ba952123ffd3fda20554c6f86609
Alex-PC <alexandre.stenger@live.fr>
2020-11-03 14:31:50 +0100

    ajout des fournitures

---
+++ fournitures/ER_1OCTET/er_1octet.vhd
@@ -0,0 +1,20 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity er_1octet is
+  port ( rst : in std_logic ;
+         clk : in std_logic ;
+         en : in std_logic ;
+         din : in std_logic_vector (7 downto 0) ;
+         miso : in std_logic ;
+         sclk : out std_logic ;
+         mosi : out std_logic ;
+         dout : out std_logic_vector (7 downto 0) ;
+         busy : out std_logic);
+end er_1octet;
+
+architecture behavioral_3 of er_1octet is
+
+begin
+
+end behavioral_3;
